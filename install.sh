6a46a8af7a8e3e888304a596225d5bf154a73a40
Vitaly Domnikov <dotcypress@gmail.com>
2025-10-15 13:25:44 +0300

    update workflow

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo 'ACTION=="add", KERNEL=="sd[a-z][0-9]", RUN+="/usr/bin/systemd-mount --no-block --automount=yes --collect $devnode /media/hash"' > /etc/udev/rules.d/99-hash-aoutomount.rules
+echo 'ACTION=="add", KERNEL=="sd[a-z][0-9]", RUN+="/usr/bin/systemd-mount --no-block --automount=yes --collect $devnode /media/hash"' > /etc/udev/rules.d/99-hash-automount.rules
 udevadm control --reload-rules
+
 cp -f hash /usr/local/bin/
 chmod +x /usr/local/bin/hash
+
 cp -f hash.service /etc/systemd/system/
 systemctl enable hash
 systemctl start hash
+
 echo "Hash installed"
